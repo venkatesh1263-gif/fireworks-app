a8fe261148b50cd4ffd6630997ee4bad4cbc47db
venkatesh1263-gif <venkatesh1263gif@gmail.com>
2025-09-03 12:21:17 +0530

    Initial commit - Customer app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-// src/App.js
-import React, { useState } from "react";
-import AdminPage from "./AdminPage";
 import CustomerPage from "./CustomerPage";
-import "./AdminPage.css";
-import "./CustomerPage.css";
 
 function App() {
-  const [isAdmin, setIsAdmin] = useState(true);
-
-  return (
-    <div className="App">
-      {isAdmin ? (
-        <AdminPage onSwitch={() => setIsAdmin(false)} />
-      ) : (
-        <CustomerPage onSwitch={() => setIsAdmin(true)} />
-      )}
-    </div>
-  );
+  return <CustomerPage />;
 }
 
 export default App;
